929b889ad8827b8de7b04a49176cbea93beeb470
djCrafter <godhedin2000@gmail.com>
2019-05-20 22:29:03 +0300

    First commit

---
+++ CurrencyConverter/Interactor.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CurrencyConverter
+{
+    class Interactor
+    {
+        Reqester reqester = new Reqester();
+
+        private void CreateTimer()
+        {
+           
+        }
+        public Interactor()
+        {
+           
+        }
+
+        async public Task<string> GetCourse(string atr1, string atr2, string value)
+        {
+            double course = await reqester.ReqestAsync(atr1, atr2);
+
+            double doubleValue;
+
+            if(double.TryParse(value, out doubleValue))
+            {
+                return (course * doubleValue).ToString();
+            }
+            else
+            {
+                return "Error!!!";
+            }
+        }
+    }
+}
---
+++ CurrencyConverter/Reqester.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using Newtonsoft.Json.Linq;
+using System.IO;
+
+namespace CurrencyConverter
+{
+    class Reqester
+    {
+            public Reqester()
+            {
+            }
+     
+            public async Task<double> ReqestAsync(string atr1, string atr2)
+            {
+                string url = $"https://free.currconv.com/api/v7/convert?q={atr1}_{atr2}&compact=ultra&apiKey=f731dbae8f77ddac4d07";
+           
+                WebRequest request = WebRequest.Create(url);
+
+             WebResponse response = await request.GetResponseAsync();
+
+                using (StreamReader stream = new StreamReader(response.GetResponseStream()))
+                {
+                    string str;
+                    if ((str = stream.ReadToEnd()) != null)
+                    {
+                        JObject o = JObject.Parse(str);
+                        return Convert.ToDouble(o[$"{atr1}_{atr2}"]);
+                    }
+                }
+                return -1;
+            }
+        
+    }
+}
